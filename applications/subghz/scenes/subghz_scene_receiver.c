11fe9b96f7dc2b337e4ba2f4d7b78b53f3c7606e
Daniel Isenhower <daniel@reciprocityentertainment.com>
2022-07-06 09:51:25 -0500

    subghz auto-detect raw defaults to off

--- applications/subghz/scenes/subghz_scene_receiver.c
+++ applications/subghz/scenes/subghz_scene_receiver.c
@@ -109,13 +109,6 @@ void subghz_scene_receiver_on_enter(void* context) {
         subghz->txrx->hopper_state = SubGhzHopperStateRunnig;
         subghz_history_reset(subghz->txrx->history);
         subghz->txrx->rx_key_state = SubGhzRxKeyStateStart;
-
-        subghz_receiver_set_filter(subghz->txrx->receiver, SubGhzProtocolFlag_Decodable | SubGhzProtocolFlag_RAW);
-
-        subghz_protocol_decoder_raw_set_auto_mode(
-            subghz_receiver_search_decoder_base_by_name(subghz->txrx->receiver, SUBGHZ_PROTOCOL_RAW_NAME),
-            true
-        );
     }
 
     subghz_view_receiver_set_lock(subghz->subghz_receiver, subghz->lock);
